844758698d4e13d1b4d31591a6ab0b08f47818f9
Caio Ribeiro <kaio03693690@gmail.com>
2022-11-29 08:54:48 -0300

    remove snap scroll and dark mode of view tech stack btton

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,8 @@ export default function Home(props: HomeProps) {
               View Projects
             </button>
             <button
-              className="text-gray-800 bg-white hover:bg-gray-50 border border-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-8 py-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              // className="text-gray-800 bg-white hover:bg-gray-50 border border-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-8 py-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+              className="text-gray-800 bg-white hover:bg-gray-50 border border-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-8 py-3"
               onClick={() => {
                 techStackSectionRef.current?.scrollIntoView({
                   behavior: "smooth",
