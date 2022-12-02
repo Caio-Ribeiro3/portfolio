85ff0f72ef27d1e54dc7d18d2c48fca44508c0dd
Caio Ribeiro <kaio03693690@gmail.com>
2022-12-02 12:36:36 -0300

    stop rotation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   const getLayout = Component.getLayout || ((page) => page)
+
+  useEffect(() => {
+    const callback = () => {
+      window.screen.orientation.lock("portrait-primary")
+    }
+
+    window.screen.orientation.addEventListener('change', callback)
+
+    return () => {
+      window.screen.orientation.removeEventListener('change', callback)
+    }
+
+  }, [])
+
 
   return getLayout(<Component {...pageProps} />)
 }
